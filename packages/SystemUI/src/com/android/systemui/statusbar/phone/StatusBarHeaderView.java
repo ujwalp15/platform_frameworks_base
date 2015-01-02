c727526a95de6418768a6a8c752952a0dd4c07c6
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-02 00:16:07 +0000

    SystemUI: extend qs haptic feedback option to Status Bar Header
    also add it to detailed view toggle and buttons
    
    Change-Id: If2b942fade80205b9de8be2a3783110bcf44fb48
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -525,6 +525,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 mActivityStarter.startActivity(showIntent.getIntent(), true /* dismissShade */);
             }
         }
+        mQSPanel.vibrateTile(20);
     }
 
     private void startSettingsActivity() {
