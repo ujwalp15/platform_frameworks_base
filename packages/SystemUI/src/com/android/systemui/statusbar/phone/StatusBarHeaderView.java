774cc500942cf8dd1af094cf5f809d9a373642cf
Adnan Begovic <adnan@cyngn.com>
2015-01-15 00:26:28 -0800

    Keyguard: Create lockscreen weather, move weatherimpl to utils.
    *also includes "SystemUI: Remove redundant and broken setText on temperture view."
    https://github.com/Euphoria-OS-Legacy/android_frameworks_base/commit/e0875874c4b45c1619f5a2fe369092079e1ecd94
    
    Change-Id: I08bae52a299ddcf9c9d0572ff97c1f927fb8e42f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -51,6 +51,8 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.internal.util.cm.WeatherController;
+import com.android.internal.util.cm.WeatherControllerImpl;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryLevelTextView;
@@ -62,8 +64,6 @@ import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.NetworkControllerImpl.EmergencyListener;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.UserInfoController;
-import com.android.systemui.statusbar.policy.WeatherController;
-import com.android.systemui.statusbar.policy.WeatherControllerImpl;
 import com.android.systemui.tuner.TunerService;
 
 import java.text.NumberFormat;
