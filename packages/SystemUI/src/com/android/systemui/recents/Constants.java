411355801b0cfc86b572bf64e7e4a3c71ab9d19a
darkobas <darkobas@gmail.com>
2015-01-25 14:22:20 -0500

    Recents: Clear All bot, and make room for it.
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml
    
    Change-Id: I1010b5a087ba9da2196c8143f6502dc65f9073e1

--- packages/SystemUI/src/com/android/systemui/recents/Constants.java
+++ packages/SystemUI/src/com/android/systemui/recents/Constants.java
@@ -56,6 +56,8 @@ public class Constants {
             // clear button location
             public static final int RECENTS_CLEAR_ALL_TOP_RIGHT = 0;
             public static final int RECENTS_CLEAR_ALL_TOP_LEFT = 1;
+            public static final int RECENTS_CLEAR_ALL_BOTTOM_RIGHT = 2;
+            public static final int RECENTS_CLEAR_ALL_BOTTOM_LEFT = 3;
         }
     }
 
