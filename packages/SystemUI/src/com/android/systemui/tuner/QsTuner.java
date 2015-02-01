33aee00e1fe92d55da7ff42b922eb1d72b6a85a1
BestPig <bestpig82@gmail.com>
2015-02-01 03:04:41 +0100

    SystemUI: add USB Tether tile
    Drawables by blunden
    
    Squashed commit:
    
    Author: mikeNG
    SystemUI: Add missing QS accessibility messages
    https://github.com/CyanogenMod/android_frameworks_base/commit/4c16334616a85197f89b74b602d87c620aa48e8e
    
    Modified for aosp QS tuner
    
    Change-Id: I28b5c098a49e103fb5f28e2ae2238da87b34f7b6

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -206,6 +206,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("volume")) return R.string.quick_settings_volume_panel_label;
         else if (spec.equals("headsup")) return R.string.quick_settings_heads_up_label;
         else if (spec.equals("tentacles")) return R.string.quick_settings_tentacles_label;
+        else if (spec.equals("usb_tether")) return R.string.quick_settings_usb_tether_label;
         return 0;
     }
 
@@ -451,6 +452,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("volume")) return R.drawable.ic_qs_volume_panel;
             else if (mSpec.equals("headsup")) return R.drawable.ic_qs_heads_up_on;
             else if (mSpec.equals("tentacles")) return R.drawable.ic_qs_tentacles;
+            else if (mSpec.equals("usb_tether")) return R.drawable.ic_qs_usb_tether_off;
             return R.drawable.android;
         }
 
