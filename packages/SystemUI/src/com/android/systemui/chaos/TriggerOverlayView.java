1ebe0347f784aaab9488f4a1f7973f4efd4a8f1d
Don <cjkacz@gmail.com>
2015-04-07 20:51:01 -0400

    FWB: Gesture Anywhere - bring it back [1/2] AICPfy lp5.0: - Lollipopify LOL - By request from doc - Added TriggerOverlayView from AppSidebar - some lines from Locskcreen gestures to get   it compiled and working
    Change-Id: I04acb9a88fe0011921a1bafdc1919d76ce57aedf

--- packages/SystemUI/src/com/android/systemui/chaos/TriggerOverlayView.java
+++ packages/SystemUI/src/com/android/systemui/chaos/TriggerOverlayView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.systemui.octos;
+package com.android.systemui.chaos;
 
 
 import android.app.KeyguardManager;
