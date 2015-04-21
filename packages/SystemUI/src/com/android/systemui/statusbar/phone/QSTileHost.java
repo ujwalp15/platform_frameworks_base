43d3b6aa9d3123cd6dc3966a9054ed66e6ccb969
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    Add Heads up QS Tile
    PureNexus Edits: adapt for nougat
    
    Change-Id: Ifc18d55153400b5473c1b68f3c64d5e3f8559f0b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -452,7 +453,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("ime")) return new ImeTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
         else if (tileSpec.equals("tentacles")) return new TentaclesTile(this);
-        else if (tileSpec.equals("night")) return new NightDisplayTile(this);
+        else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
