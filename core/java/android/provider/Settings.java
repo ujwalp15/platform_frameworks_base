38a83255ed263ee9edba8873f1061ea2721db908
Don <cjkacz@gmail.com>
2015-08-23 22:59:24 -0400

    Enhance power menu reboot option (1,2) With global reboot option Add global reboot option when long press power key and confirmation dialog when user chooses to reboot device.
    Change-Id: Iea247c79488bfa38c7d12344541bb3e041834925

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4899,6 +4899,12 @@ public final class Settings {
         public static final String SLEEP_TIMEOUT = "sleep_timeout";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -4948,7 +4954,8 @@ public final class Settings {
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
-            SLEEP_TIMEOUT
+            SLEEP_TIMEOUT,
+            ADVANCED_REBOOT
         };
 
         /**
@@ -5340,6 +5347,12 @@ public final class Settings {
          * Whether ADB is enabled.
          */
         public static final String ADB_ENABLED = "adb_enabled";
+
+        /**
+         * String to contain power menu actions
+         * @hide
+         */
+        public static final String POWER_MENU_ACTIONS = "power_menu_actions";
 
         /**
          * Whether Views are allowed to save their attribute data.
