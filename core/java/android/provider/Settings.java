a86ad8cbd8d0fc7a5b2d214d4ada5e2a23bbdbc7
Don <cjkacz@gmail.com>
2015-09-07 11:05:16 -0400

    frameworks/base: Bringup expanded desktop and implement per-user conf… …iguration.
    Change-Id: Id5bce0ccef2e03758ff3278567f51cfe97da7825

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6808,14 +6808,15 @@ public final class Settings {
          */
         public static final String WIFI_BOUNCE_DELAY_OVERRIDE_MS = "wifi_bounce_delay_override_ms";
 
-        /**
+         /**
          * Defines global runtime overrides to window policy.
          *
-         * See {@link com.android.internal.policy.impl.PolicyControl} for value format.
+         * See {@link android.view.WindowManagerPolicyControl} for value format.
          *
          * @hide
          */
         public static final String POLICY_CONTROL = "policy_control";
+        public static final String POLICY_CONTROL_SELECTED = "policy_control_selected";
 
         /**
          * Defines global zen mode.  ZEN_MODE_OFF, ZEN_MODE_IMPORTANT_INTERRUPTIONS,
