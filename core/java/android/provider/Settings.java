bc128dc0bfbe2092c8a91b87d2138a924a07e7a2
Max Weninger <max.weninger@gmail.com>
2016-10-14 08:56:30 +0200

    Add OmniSwitch as option for default recents [1/2]
    Change-Id: Ia6398a1d463d36b039d6a222b1db4f8478794bc6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3684,7 +3684,7 @@ public final class Settings {
         public static final Validator POINTER_SPEED_VALIDATOR =
                 new InclusiveFloatRangeValidator(-7, 7);
 
-    /**
+        /**
          * Whether the phone volume up / down effect should be played
          * @hide
          */
@@ -3704,6 +3704,12 @@ public final class Settings {
          * @hide
          */
         public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
+         * Alternative recent apps integration using OmniSwitch
+         * @hide
+         */
+        public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
 
         /**
          * I am the lolrus.
@@ -4042,7 +4048,7 @@ public final class Settings {
          */
         public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY = "proximity_auto_speaker_incall_only";
 
-       /**
+        /**
          * Enable looking up of phone numbers of nearby places
          *
          * @hide
