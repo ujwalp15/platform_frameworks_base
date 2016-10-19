7438a57b5797b408526657dfb0df93e63f32f7f5
beanstown106 <nbenis106@gmail.com>
2016-10-19 07:26:10 +0200

    Custom Carrier Label & Carrier Label Placement [1/2]
    Squash commits:
    
    Keyguard/Statusbar Carrier label options [1/2]
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    * code originally writted by @Altaf-Mahdi i just squased into one commit
    PureNexusProject-Legacy/android_frameworks_base@688d56f
    
    Show carrier label / custom & change color [1/2]
    *code originally written by @martincz
    *includes various fixs by the du team
    PureNexusProject-Legacy/android_frameworks_base@16a247a
    
    Carrier label - fix capitilization bug
    PureNexusProject-Legacy/android_frameworks_base@4b9e22b
    
    remove carrier label color and make carrier label play nice with dark statusbar
    PureNexusProject-Legacy/android_frameworks_base@3f32102
    
    Fix Carrier Label font size not changing on system font size change
    DirtyUnicorns/android_frameworks_base@8da9dcf
    
    include carrier label in icon merger width calculation
    PureNexusProject-Legacy/android_frameworks_base@9df3a0c
    
    Credit/Thanks to @Altaf-Mahdi @beanstown106 @dwitherell and @martincz
    
    PureNexus Edits:
    - Adapt statusbar hook for N
    
    Change-Id: I0a26060afb740c359a9b4a722945d295bd181a4d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -358,6 +358,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     View mExpandedContents;
     TextView mNotificationPanelDebugText;
 
+    // Custom Carrier Label
+    private int mShowCarrierLabel;
+    private TextView mCarrierLabel;
+
     // settings
     private QSPanel mQSPanel;
 
@@ -439,6 +443,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                    Settings.Secure.QS_COLUMNS),
                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                   Settings.System.STATUS_BAR_SHOW_CARRIER),
+                   false, this, UserHandle.USER_ALL);
+           updateSettings();
         }
 
         @Override
@@ -452,7 +460,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             } else if (uri.equals(Settings.Secure.getUriFor(
                     Settings.Secure.QS_COLUMNS))) {
                     updateResources();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_SHOW_CARRIER))) {
+                    updateCarrier();
             }
+            updateSettings();
+        }
+
+        public void updateSettings() {
+            ContentResolver resolver = mContext.getContentResolver();
+
+            mShowCarrierLabel = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_SHOW_CARRIER, 1, UserHandle.USER_CURRENT);
         }
     }
 
@@ -940,6 +959,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         initSignalCluster(mStatusBarView);
         initSignalCluster(mKeyguardStatusBar);
         initEmergencyCryptkeeperText();
+
+        mCarrierLabel = (TextView) mStatusBarWindow.findViewById(R.id.statusbar_carrier_text);
+        if (mCarrierLabel != null) {
+            updateCarrier();
+        }
 
         mFlashlightController = new FlashlightController(mContext);
         mKeyguardBottomArea.setFlashlightController(mFlashlightController);
@@ -2040,6 +2064,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public void requestNotificationUpdate() {
         updateNotifications();
+    }
+
+    private void updateCarrier() {
+        if (mCarrierLabel != null) {
+            if (mShowCarrierLabel == 2) {
+                mCarrierLabel.setVisibility(View.VISIBLE);
+            } else if (mShowCarrierLabel == 3) {
+                mCarrierLabel.setVisibility(View.VISIBLE);
+            } else {
+                mCarrierLabel.setVisibility(View.GONE);
+            }
+        }
     }
 
     @Override
@@ -4394,6 +4430,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateStackScrollerState(goingToFullShade, fromShadeLocked);
         updateNotifications();
         checkBarModes();
+        updateCarrier();
         updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
         mKeyguardMonitor.notifyKeyguardState(mStatusBarKeyguardViewManager.isShowing(),
                 mStatusBarKeyguardViewManager.isSecure(),
