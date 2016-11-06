38af8d1ad89213b926d6755ea1cab8630c9f32ba
Andrew Boren <andrew.boren@gmail.com>
2016-11-06 01:16:45 +0100

    Screenshot delay [1/2]
    **DU Edits**
    
    Pulled from Evervolv's Extended powermenu mod back in Lollipop, we've gone ahead and extended
    this function to the QS tile and the vol+power gesture. Tweaked also to use a seekbar instead
    of their number preference which is nice but I like seekbars now :-)
    
    https://github.com/Evervolv/android_frameworks_base/commit/ed3d0763acdf0ac44dbcd0efaf1c472dadf90922
    
    Change-Id: If238089673a42fce79d2d3ee3ff930793c6c69d1

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -715,6 +715,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private int mCurrentUserId;
 
+    private int mScreenshotDelay;
+
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
@@ -1083,6 +1085,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && (event.getFlags() & KeyEvent.FLAG_FALLBACK) == 0) {
             mPowerKeyTriggered = true;
             mPowerKeyTime = event.getDownTime();
+            checkSettings();
             interceptScreenshotChord();
             interceptScreenrecordChord();
         }
@@ -5678,6 +5681,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         };
                         msg.replyTo = new Messenger(h);
                         msg.arg1 = msg.arg2 = 0;
+
+                        // Needs delay or else we'll be taking a screenshot of the dialog each time
+                        try {
+                            Thread.sleep(mScreenshotDelay);
+                        } catch (InterruptedException ie) {
+                            // Do nothing
+                        }
+
                         if (mStatusBar != null && mStatusBar.isVisibleLw())
                             msg.arg1 = 1;
                         if (mNavigationBar != null && mNavigationBar.isVisibleLw())
@@ -5888,6 +5899,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             mScreenshotChordVolumeDownKeyConsumed = false;
                             cancelPendingPowerKeyAction();
                             cancelPendingScreenrecordChordAction();
+                            checkSettings();
                             interceptScreenshotChord();
                         }
                     } else {
@@ -8219,5 +8231,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mKeyguardDelegate != null) {
             mKeyguardDelegate.dump(prefix, pw);
         }
+    }
+
+    private void checkSettings() {
+        mScreenshotDelay = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_DELAY, 1000);
     }
 }
