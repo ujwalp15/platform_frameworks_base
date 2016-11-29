f376434943c82ef0e37e351bdff6b12a71626e0d
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-29 08:00:41 -0500

    Battery: allow set a custom symbol near text pct on charging [1/2]
    default (disabled)
    flash
    tilde
    
    PS2 - OCD things
    
    Change-Id: If15530d5dc7fef4d40eab3c21ac46ee6f27f9bca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6929,7 +6929,7 @@ public final class Settings {
         public static final String CAMERA_DOUBLE_TWIST_TO_FLIP_ENABLED =
                 "camera_double_twist_to_flip_enabled";
 
-       /**
+        /**
          * Status bar battery %
          * 0: Hide the battery percentage
          * 1: Display the battery percentage inside the icon
@@ -6957,18 +6957,28 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE_TILE = "status_bar_battery_style_tile";
 
         /**
-          * battery icon color when charging
-          *
-          * @hide
-          */
-         public static final String STATUS_BAR_CHARGE_COLOR = "status_bar_charge_color";
-
-        /**
-         * Whether to force percentage text out of the battery icon when charging
+         * Battery icon color while charging
          *
          * @hide
          */
-         public static final String FORCE_CHARGE_BATTERY_TEXT = "force_charge_battery_text";
+         public static final String STATUS_BAR_CHARGE_COLOR = "status_bar_charge_color";
+
+        /**
+         * Whether to force percentage text out of the battery icon while charging
+         *
+         * @hide
+         */
+        public static final String FORCE_CHARGE_BATTERY_TEXT = "force_charge_battery_text";
+
+        /**
+         * Charging symbol near battery text percentage
+         * 0: no symbol
+         * 1: flash symbol
+         * 2: tilde symbol
+         * default: 0
+         * @hide
+         */
+        public static final String TEXT_CHARGING_SYMBOL = "text_charging_symbol";
 
         /**
          * Control whether Night display is currently activated.
