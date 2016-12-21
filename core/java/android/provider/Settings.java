fcb2278dbb724e881552fd5bbff4225f7892851d
Janson Kang <temasek71@gmail.com>
2016-12-21 23:20:19 +0100

    FWB: Clock font style (SQUASHED)
    Change-Id: I1d9c29a336daef00a36df651f56fa73a738153cc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3790,6 +3790,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
         /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
