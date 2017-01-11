a34024e11e966386e8bb7e40cda474d004128192
Joe Maples <joe@frap129.org>
2017-01-11 17:19:28 -0500

    [1/2] Reload Pixel Home Animation without reboot
    @beanstown104 this shit hot
    
    Change-Id: I17c274f3715d2f3d93f7c121f32b3f719db81c11
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
+++ packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -2,8 +2,13 @@ package com.google.android.systemui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -83,6 +88,28 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
     private final Interpolator mFastOutSlowInInterpolator;
     private final Interpolator mHomeDisappearInterpolator;
 
+    private SettingsObserver mSettingsObserver;
+
+    protected class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+           ContentResolver resolver = mContext.getContentResolver();
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.PIXEL_NAV_ANIMATION),
+                  false, this, UserHandle.USER_CURRENT);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+           super.onChange(selfChange, uri);
+           setOpaEnabled(true);
+        }
+
+    }
+
     public OpaLayout(Context context) {
         super(context);
         this.mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
@@ -106,8 +133,13 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
                 OpaLayout.this.startRetractAnimation();
             }
         };
-        this.mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
-        this.mCurrentAnimators = new ArraySet<Animator>();
+        mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
+        mCurrentAnimators = new ArraySet<Animator>();
+
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     public OpaLayout(Context context, AttributeSet attrs) {
@@ -133,8 +165,13 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
                 OpaLayout.this.startRetractAnimation();
             }
         };
-        this.mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
-        this.mCurrentAnimators = new ArraySet<Animator>();
+        mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
+        mCurrentAnimators = new ArraySet<Animator>();
+
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -160,8 +197,13 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
                 OpaLayout.this.startRetractAnimation();
             }
         };
-        this.mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
-        this.mCurrentAnimators = new ArraySet<Animator>();
+        mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
+        mCurrentAnimators = new ArraySet<Animator>();
+
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -187,8 +229,13 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
                 OpaLayout.this.startRetractAnimation();
             }
         };
-        this.mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
-        this.mCurrentAnimators = new ArraySet<Animator>();
+        mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
+        mCurrentAnimators = new ArraySet<Animator>();
+
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     private void startAll(ArraySet<Animator> animators) {
