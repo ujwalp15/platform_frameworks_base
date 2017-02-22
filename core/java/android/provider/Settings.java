579b5a8200fbf2ca6aa0b580c5e36724da1768db
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-02-22 11:47:27 -0500

    DUI Smartbar: double tap to sleep on navbar [1/3]
    Change-Id: Ib90c61348e88e5b752eddeb0d83685db360de6d3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3965,6 +3965,12 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         *  Enable smartbar double tap to sleep gesture
+         * @hide
+         */
+        public static final String SMARTBAR_DOUBLETAP_SLEEP = "smartbar_doubletap_sleep";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
